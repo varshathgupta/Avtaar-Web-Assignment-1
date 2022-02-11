7c24e25cc96bf1e41e673483d7164bee36baa23e
varshathgupta <varshathgupta@gmail.com>
2022-02-11 22:20:51 +0530

    component1 created

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './navbar.css'
+import logo from '../../images/logo.png'
+function Navbar() {
+  return (
+      <>
+      <container className="navbar">
+      <img className="logo" src={logo} alt="log" />
+      <a href="#" className="link1">
+       ASSOCIATION  
+      </a>
+      <a href="#" className="link2">
+       INTERNSHIP
+      </a>
+      <a href="#" className="link3">
+       ABOUT US 
+      </a>
+      <a href="#" className="link4">
+       CONTACT US  
+      </a>
+      
+
+      
+      </container>
+      </>
+  )
+}
+
+export default Navbar;
